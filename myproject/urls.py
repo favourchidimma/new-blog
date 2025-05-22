d9d0c157305bd2e7e04738764a0f9f31b7653c11
favourchidimma <idikafavourchidimma1@gmail.com>
2025-05-22 10:04:37 +0100

    Changed my api name

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,7 +24,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
-      title="Blog API",
+      title="Idika API",
       default_version='v1',
       description="This is my blog API doc",
       terms_of_service="",
